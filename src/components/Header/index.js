327beefb6c51c226b8df621b9b53c60271480c02
Vladimir Sevastianov <108.nimay@gmail.com>
2021-03-11 19:22:23 +0200

    init: second task start

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="root">
+      <div className="forest"></div>
+      <div className="container">
+          <h1>This is title</h1>
+          <p>This is Description!</p>
+      </div>
+    </header>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,13 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
+
+
+
+
+
+
+
+
+const AppList = () => {
+  return (
+    <ul>
+      <li>My first list</li>
+      <li>My second list</li>
+    </ul>
+  );
+};
+
+const AppHeader = ({title, hideBg = false}) => {
+  const bull = undefined;
+  const styleRoot = hideBg ? {color: 'yellow'} : {};
+  return (
+    <>
+      <label htmlFor="idshnik">label</label>
+
+      {
+        title && (<h1 id="idshnik" hidden={bull} tabIndex={bull ? 1 : 0} className="header" style={styleRoot}>
+          Hello, World, {bull ? "yes" : "no"} React.js! {title}
+        </h1>)
+      }
+
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      <AppHeader
+        title='my Krishna title'
+        hideBg
+      />
+      <AppList />
+    </>
+  );
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
+
+console.log(typeof React);
