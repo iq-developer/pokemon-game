9235fb514141915013bc02c8ce1bd734ff10df3b
Vladimir Sevastianov <108.nimay@gmail.com>
2021-03-18 21:19:18 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import {useState} from 'react';
 import GamePage from './routes/Game';
 import HomePage from './routes/Home';
 
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
 const App = () => {
   const [page, setPage] = useState('app');
 
@@ -10,14 +12,26 @@ const App = () => {
     setPage(page);
   }
 
-  switch (page) {
-    case 'app':
-      return <HomePage onChangePage={handleChangePage} />
-    case 'game':
-      return <GamePage />
-    default:
-      return <HomePage />
-  }
+  <BrowserRouter>
+    <Switch>
+      <Route path='/' exact component={HomePage} />
+      <Route path='/game' component={GamePage} />
+      <Route render={() => {
+        <h1>404 non found</h1>
+      }} />
+    </Switch>
+  </BrowserRouter>
+
+
+  // switch (page) {
+  //   case 'app':
+  //     return <HomePage onChangePage={handleChangePage} />
+  //   case 'game':
+  //     return <GamePage />
+  //   default:
+  //     return <HomePage />
+  // }
+
 }
 
 export default App;
