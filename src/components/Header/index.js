bfdaa374da2125157b2480f6f002bdf7d304b279
Vladimir Sevastianov <108.nimay@gmail.com>
2021-03-23 21:42:18 +0200

    feat: start homework-7

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,6 +9,8 @@ const Header = ({title, descr, onClickButton}) => {
   return (
     <header className={style.root}>
       <div className={style.forest}></div>
+      <div className={style.silhouette}></div>
+      <div className={style.moon}></div>
       <div className={style.container}>
           <h1>{title}</h1>
           <p>{descr}</p>
