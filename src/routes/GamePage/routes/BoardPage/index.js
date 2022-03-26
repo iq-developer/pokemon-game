4040ea43ca1585dd73d72f5badb43fb53b80325f
Vladimir Sevastianov <108.nimay@gmail.com>
2022-03-26 21:12:36 +0200

    feat: create player2

--- src/routes/GamePage/routes/BoardPage/index.js
+++ src/routes/GamePage/routes/BoardPage/index.js
@@ -6,14 +6,20 @@ import s from './style.module.css';
 
 const BoardPage = () => {
   const [board, setBoard] = useState([]);
+  const [player2, setPlayer2] = useState([]);
   const { pokemons } = useContext(PokemonContext);
   const history = useHistory();
-  console.log('board: ', board);
+
+  console.log('player2: ', player2);
+
   useEffect(async () => {
     const boardResponse = await fetch('https://reactmarathon-api.netlify.app/api/board');
     const boardRequest = await boardResponse.json();
     setBoard(boardRequest.data);
 
+    const player2Response = await fetch('https://reactmarathon-api.netlify.app/api/create-player');
+    const player2Request = await player2Response.json();
+    setPlayer2(player2Request.data);
   }, []);
 
   if (Object.keys(pokemons).length === 0) {
@@ -45,6 +51,7 @@ const BoardPage = () => {
           ))
         }
       </div>
+
       <div className={s.board}>
         {
           board.map((item) => (
@@ -60,6 +67,25 @@ const BoardPage = () => {
           ))
         }
       </div>
+
+      <div className={s.playerTwo}>
+        {
+          player2.map(({ name, img, id, type, values }) => (
+            <PokemonCard
+              className={s.card}
+              key={id}
+              id={id}
+              name={name}
+              type={type}
+              values={values}
+              img={img}
+              minimize
+              isActive
+            />
+          ))
+        }
+      </div>
+
     </div>
   );
 };
