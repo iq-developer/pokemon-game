a9aa4e9953517c478ed903a2bbf09f75211e497b
Vladimir Sevastianov <108.nimay@gmail.com>
2022-03-30 17:58:06 +0300

    fix: logo link to homepage

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,17 +1,25 @@
 import s from './style.module.css';
 import cn from 'classnames';
+import { Link } from 'react-router-dom';
 
-const Navbar = ({isOpen, bgActive = false, onClickHamburg}) => {
+const Navbar = ({ isOpen, bgActive = false, onClickHamburg }) => {
+
   return (
     <nav className={cn(s.root, {
       [s.bgActive]: bgActive
     })}>
-      <div className={s.navWrapper} onClick={onClickHamburg}>
-        <p className={s.brand}>
-          Pokemons
+      <div className={s.navWrapper} >
+        <p>
+          <Link to={'/'}>
+            <span className={s.brand}>
+              Pokemons
+            </span>
+          </Link>
         </p>
-        <div className={cn(s.menuButton, {
-          [s.active]: isOpen
+        <div
+          onClick={onClickHamburg}
+          className={cn(s.menuButton, {
+            [s.active]: isOpen
           })}>
           <span />
         </div>
